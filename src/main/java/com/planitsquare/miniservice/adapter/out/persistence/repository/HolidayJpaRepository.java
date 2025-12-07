cd037e5b89d5aa0b128138bdb02ce5c276d08c56
uhanuu <hwryu990927@gmail.com>
2025-12-07 14:46:08 +0900

    feat(adapter): 공휴일 삭제 Adapter 구현
    특정 연도와 국가의 공휴일을 삭제하는 Adapter Layer 구현을 추가합니다.
    
    - HolidayJpaRepository: deleteByCountryCodeAndYear 쿼리 메서드 추가
    - HolidaysAdapter: DeleteHolidaysPort 구현 추가

--- src/main/java/com/planitsquare/miniservice/adapter/out/persistence/repository/HolidayJpaRepository.java
+++ src/main/java/com/planitsquare/miniservice/adapter/out/persistence/repository/HolidayJpaRepository.java
@@ -2,6 +2,20 @@ package com.planitsquare.miniservice.adapter.out.persistence.repository;
 
 import com.planitsquare.miniservice.adapter.out.persistence.entity.HolidayJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HolidayJpaRepository extends JpaRepository<HolidayJpaEntity, Long> {
+
+  /**
+   * 특정 연도와 국가 코드에 해당하는 모든 공휴일을 삭제합니다.
+   *
+   * @param countryCode 국가 코드
+   * @param year 연도
+   * @return 삭제된 레코드 수
+   */
+  @Modifying
+  @Query("DELETE FROM HolidayJpaEntity h WHERE h.country.code = :countryCode AND YEAR(h.date) = :year")
+  int deleteByCountryCodeAndYear(@Param("countryCode") String countryCode, @Param("year") int year);
 }
